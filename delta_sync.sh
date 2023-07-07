3f5c72802b1fdcacfb20a8cd0a70dc6e32c7c2b8
debjena20 <95019999+debjena20@users.noreply.github.com>
2023-07-07 14:16:23 +0530

    delta_sync.sh

--- delta_sync.sh
+++ delta_sync.sh
@@ -2,19 +2,19 @@
 
 # Script written by Deb Jena
 
-# Set the source and destination paths
+# Set source and destination paths
 SOURCE="/storage/backup-wl/delta"
 LOCAL_DESTINATION="/home/user/delta_backups"
 REMOTE_DESTINATION="/storage/backup-wl/delta_remote"
 REMOTE_SERVER="user@10.1.70.125"
 
-# Function to copy files from source to local destination
+# Function to copy files from source to local
 copy_files_locally() {
   mkdir -p "$LOCAL_DESTINATION"
   rsync -av --remove-source-files "$SOURCE/"*.zip "$LOCAL_DESTINATION"
 }
 
-# Function to copy files from local destination to remote destination
+# Function to copy files from local to remote destination
 copy_files_remotely() {
   rsync -av -e ssh "$LOCAL_DESTINATION/"*.zip "$REMOTE_SERVER:$REMOTE_DESTINATION" --rsync-path="sudo rsync"
 }
@@ -25,7 +25,7 @@ delete_files_locally() {
   find "$LOCAL_DESTINATION" -name "*.zip" -type f -mtime +"$retention_period" -exec rm {} \;
 }
 
-# Call the functions
+# Call functions
 copy_files_locally
 copy_files_remotely
 delete_files_locally 7
