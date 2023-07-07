93ef37d8f6878f300a0f06845045c86580b4d43a
debjena20 <95019999+debjena20@users.noreply.github.com>
2023-07-07 14:22:33 +0530

    Update delta_sync.sh

--- delta_sync.sh
+++ delta_sync.sh
@@ -6,15 +6,23 @@
 SOURCE="/storage/backup-wl/delta"
 LOCAL_DESTINATION="/home/user/delta_backups"
 REMOTE_DESTINATION="/storage/backup-wl/delta_remote"
-REMOTE_SERVER="user@10.1.70.125"
+REMOTE_SERVER="user@10.1.24.90"
 
-# Function to copy files from source to local
+# Function to copy files from source to local server
 copy_files_locally() {
   mkdir -p "$LOCAL_DESTINATION"
+
+  # Check if there are any files in the source directory
+  files=$(find "$SOURCE" -maxdepth 1 -name "*.zip" -type f)
+  if [[ -z "$files" ]]; then
+    echo "No files found in $SOURCE. Skipping the copy operation."
+    return
+  fi
+
   rsync -av --remove-source-files "$SOURCE/"*.zip "$LOCAL_DESTINATION"
 }
 
-# Function to copy files from local to remote destination
+# Function to copy files from local to remote server
 copy_files_remotely() {
   rsync -av -e ssh "$LOCAL_DESTINATION/"*.zip "$REMOTE_SERVER:$REMOTE_DESTINATION" --rsync-path="sudo rsync"
 }
